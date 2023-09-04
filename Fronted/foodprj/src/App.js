b872ea8872f8c0594c7efa9078f34abb58040da8
Sambond22 <sb2002sati@gmail.com>
2023-09-04 21:29:28 +0530

    fronted

---
+++ Fronted/foodprj/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./Components/Layout/Header"
+import {Home} from "./Components/Home"
+import {Footer} from "./Components/Layout/Footer"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ Fronted/foodprj/src/Components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+        <div className='py-1'>
+            <p className='text-centre mt-2'>
+                Food Delivery Website --2023-2024, All Rights Reserved
+            </p>
+        </div>
+    </div>
+  )
+}
